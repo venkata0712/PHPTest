9dacffbefd56f368fa743af79b8a9ef651db7280
venkat <venkat.subrahmanyam@shanrohi.com>
2019-07-20 11:58:35 +0530

    Ë:/PHPTest/TestingProj

---
+++ src/php1/pp1/test1.java
@@ -0,0 +1,27 @@
+package php1.pp1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class test1 {
+
+	public static void main(String[] args) throws Exception {
+		System.setProperty("webdriver.chrome.driver","c:\\softwares\\chromedriver.exe");
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("http://13.59.84.32/PHPTest/index.php");
+		
+		driver.findElement(By.xpath("/html/body/form/table/tbody/tr[1]/td[2]/input")).sendKeys("ASD");
+		driver.findElement(By.xpath("/html/body/form/table/tbody/tr[2]/td[2]/input")).sendKeys("rohit");
+		driver.findElement(By.xpath("/html/body/form/table/tbody/tr[3]/td[2]/input")).sendKeys("Virat");
+		driver.findElement(By.xpath("/html/body/form/table/tbody/tr[4]/td[2]/input")).sendKeys("123");
+		driver.findElement(By.xpath("/html/body/form/table/tbody/tr[5]/td[2]/input")).sendKeys("123");
+		driver.findElement(By.xpath("/html/body/form/table/tbody/tr[6]/td[2]/input")).sendKeys("mail@mail.com");
+		driver.findElement(By.xpath("/html/body/form/table/tbody/tr[7]/td[2]/input[1]")).sendKeys(" ");
+		driver.findElement(By.xpath("/html/body/form/table/tbody/tr[8]/td/input[1]")).sendKeys(" ");
+		
+		driver.findElement(By.xpath("/html/body/form/table/tbody/tr[8]/td/input[2]")).click();
+	}
+	
+	
+}
+
