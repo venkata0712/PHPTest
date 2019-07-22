8e8681280bdc2c4f2e24bcf65172a9db18564d02
venkat <venkat.subrahmanyam@shanrohi.com>
2019-07-22 15:41:39 +0530

    Modified 9

--- src/php1/pp1/test1.java
+++ src/php1/pp1/test1.java
@@ -17,32 +17,32 @@ public class test1 {
 		chromeOptions.addArguments("--headless", "--disable-gpu", "--window-size=1920,1200","--ignore-certificate-errors");  
 		WebDriver driver = new ChromeDriver(chromeOptions);  
 		
-//		Thread.sleep(2000);
-//		driver.get("http://13.59.84.32/PHPTest/index.php");
-//		Thread.sleep(3000);
-//		driver.findElement(By.xpath("/html/body/form/table/tbody/tr[1]/td[2]/input")).sendKeys("ASD");
-//		driver.findElement(By.xpath("/html/body/form/table/tbody/tr[2]/td[2]/input")).sendKeys("rohit");
-//		driver.findElement(By.xpath("/html/body/form/table/tbody/tr[3]/td[2]/input")).sendKeys("Virat");
-//		driver.findElement(By.xpath("/html/body/form/table/tbody/tr[4]/td[2]/input")).sendKeys("123");
-//		driver.findElement(By.xpath("/html/body/form/table/tbody/tr[5]/td[2]/input")).sendKeys("123");
-//		driver.findElement(By.xpath("/html/body/form/table/tbody/tr[6]/td[2]/input")).sendKeys("mail@mail.com");
-//		driver.findElement(By.xpath("/html/body/form/table/tbody/tr[7]/td[2]/input[1]")).sendKeys(" ");
-//		driver.findElement(By.xpath("/html/body/form/table/tbody/tr[8]/td/input[1]")).sendKeys(" ");
-//
-//		driver.findElement(By.xpath("/html/body/form/table/tbody/tr[8]/td/input[2]")).click();
-//		System.out.println("Passed");
-//		driver.close();
-		
-		driver.get("https://google.com");
+		Thread.sleep(2000);
+		driver.get("http://172.31.35.123/PHPTest/index.php");
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("/html/body/form/table/tbody/tr[1]/td[2]/input")).sendKeys("ASD");
+		driver.findElement(By.xpath("/html/body/form/table/tbody/tr[2]/td[2]/input")).sendKeys("rohit");
+		driver.findElement(By.xpath("/html/body/form/table/tbody/tr[3]/td[2]/input")).sendKeys("Virat");
+		driver.findElement(By.xpath("/html/body/form/table/tbody/tr[4]/td[2]/input")).sendKeys("123");
+		driver.findElement(By.xpath("/html/body/form/table/tbody/tr[5]/td[2]/input")).sendKeys("123");
+		driver.findElement(By.xpath("/html/body/form/table/tbody/tr[6]/td[2]/input")).sendKeys("mail@mail.com");
+		driver.findElement(By.xpath("/html/body/form/table/tbody/tr[7]/td[2]/input[1]")).sendKeys(" ");
+		driver.findElement(By.xpath("/html/body/form/table/tbody/tr[8]/td/input[1]")).sendKeys(" ");
 
-		Thread.sleep(1000);
-
-		if (driver.getPageSource().contains("I'm Feeling Lucky")) {
-			System.out.println("Pass");
-		} else {
-			System.out.println("Fail");
-		}
+		driver.findElement(By.xpath("/html/body/form/table/tbody/tr[8]/td/input[2]")).click();
+		System.out.println("Passed");
 		driver.quit();
+		
+//		driver.get("https://google.com");
+//
+//		Thread.sleep(1000);
+//
+//		if (driver.getPageSource().contains("I'm Feeling Lucky")) {
+//			System.out.println("Pass");
+//		} else {
+//			System.out.println("Fail");
+//		}
+//		driver.quit();
 	}
 
 
