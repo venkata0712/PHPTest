bfb0086656440c27fc2538ea201636c80ddbe55c
venkat <venkat.subrahmanyam@shanrohi.com>
2019-07-23 17:30:48 +0530

    Modified 3

--- src/php1/pp1/test1.java
+++ src/php1/pp1/test1.java
@@ -4,11 +4,15 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class test1 {
 
 	public static void main(String[] args) throws Exception {
-		System.setProperty("webdriver.chrome.driver","/usr/bin/chromedriver");
+		//System.setProperty("webdriver.chrome.driver","/usr/bin/chromedriver");
+		System.setProperty("webdriver.chrome.driver","C://Softwares//Chromedriver.exe");
 		ChromeOptions chromeOptions = new ChromeOptions();
 //		chromeOptions.addArguments("--headless");
 //		chromeOptions.addArguments("--no-sandbox");
@@ -16,12 +20,15 @@ public class test1 {
 
 		chromeOptions.addArguments("--headless", "--disable-gpu", "--window-size=1920,1200","--ignore-certificate-errors");  
 		WebDriver driver = new ChromeDriver(chromeOptions);  
+		WebDriverWait wait = new WebDriverWait(driver, 100);
 		
 		Thread.sleep(2000);
 		System.out.println("Entered Browser");
-		driver.get("http://18.188.33.81/PHPTest/index.php");
+		driver.get("http://18.221.170.199/PHPTest/index.php");
 		System.out.println("View Browser");
 		Thread.sleep(3000);
+		//wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/form/table/tbody/tr[1]/td[2]/input")));
+		//wait.until(ExpectedConditions.elementToBeSelected(By.xpath("/html/body/form/table/tbody/tr[1]/td[2]/input")));
 		driver.findElement(By.xpath("/html/body/form/table/tbody/tr[1]/td[2]/input")).sendKeys("ASD");
 		driver.findElement(By.xpath("/html/body/form/table/tbody/tr[2]/td[2]/input")).sendKeys("rohit");
 		driver.findElement(By.xpath("/html/body/form/table/tbody/tr[3]/td[2]/input")).sendKeys("Virat");
